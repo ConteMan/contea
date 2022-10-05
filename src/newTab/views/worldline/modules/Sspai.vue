fafb5a289c4aecfc56a899e9c53fe499f5daab01
ConteMan <real.conteman@gmail.com>
2022-10-05 21:34:16 +0800

    feat: 新标签页, 世界线, 少数派模块展示适用内容布局;

--- src/newTab/views/worldline/modules/Sspai.vue
+++ src/newTab/views/worldline/modules/Sspai.vue
@@ -5,6 +5,7 @@ import { enumToObj } from '@utils/index'
 import { ConfigModel } from '@models/index'
 import Base from '@services/base'
 import Alarm from '@services/base/alarm'
+import WorldlineContent from '@newTab/layout/WorldlineContent.vue'
 
 import dayjs from 'dayjs'
 import 'dayjs/locale/zh-cn'
@@ -111,71 +112,76 @@ const banner = (type: string, item: Record<string, any>) => {
 </script>
 
 <template>
-  <div class="w-full flex">
-    <div class="flex-shrink-0 flex-grow-0 pt-10 pb-4 px-2 flex flex-col items-start gap-2">
+  <WorldlineContent>
+    <template #bar>
+      <a class="cursor-pointer py-2 px-4 flex items-center" @click="refresh()">
+        <mdi-refresh :class="{ 'animate-spin': loading }" />
+      </a>
+      <div class=" h-[30%] mx-4 border-l border-l-gray-400 opacity-20" />
       <div
         v-for="item in data.moduleTypes" :key="item.value"
-        class="py-2 px-4 cursor-pointer"
-        :class="{ 'text-red-500 font-bold': selectedTags.includes(item.value) }"
+        class="py-2 px-2 cursor-pointer opacity-60"
+        :class="{ 'text-red-500 !opacity-100': selectedTags.includes(item.value) }"
         @click="clickTab(item.value)"
       >
         {{ item.key }}
       </div>
-      <a class="cursor-pointer py-2 px-4 flex items-center" @click="refresh()">
-        <mdi-refresh :class="{ 'animate-spin': loading }" />
-      </a>
-    </div>
+    </template>
 
-    <div
-      v-infinite-scroll="[loadMore, { distance: 10 }]"
-      class="hover-scroll flex-grow overflow-y-auto mt-10 mb-4 px-6 flex flex-col gap-4"
-    >
+    <template #content>
       <div
-        v-for="item in list" :key="item.title"
-        class="max-w-[1080px] min-h-[132px] p-4 rounded-md shadow-current bg-gray-400 bg-opacity-20 flex gap-6 hover:(bg-opacity-40)"
+        v-infinite-scroll="[loadMore, { distance: 10 }]"
+        class="h-full overflow-y-auto hover-scroll pr-8 flex flex-col gap-4"
       >
-        <div class="flex-grow flex flex-col justify-center items-start gap-6">
-          <div class="text-[15px] font-bold">
-            <template v-if="['index', 'matrix'].includes(item.ca_module_type)">
-              <a :href="`${config.site}/post/${item.ca_data_id}`">
-                {{ item.data.title }}
-              </a>
-            </template>
-            <template v-if="['followActivity'].includes(item.ca_module_type)">
-              <template v-if="['like_article', 'release_article'].includes(item.data.key)">
-                <a :href="`${config.site}/post/${item.ca_data_id}`">
-                  {{ item.data.data.title }}
-                </a>
-              </template>
-            </template>
-          </div>
+        <div
+          v-for="item in list" :key="item.title"
+          class="p-4 rounded-md bg-gray-400 bg-opacity-20 hover:(bg-opacity-40)"
+        >
+          <div class="max-w-[1080px] min-h-[132px] flex gap-6">
+            <div class="flex-grow flex flex-col justify-center items-start gap-6">
+              <div class="text-[15px] font-bold">
+                <template v-if="['index', 'matrix'].includes(item.ca_module_type)">
+                  <a :href="`${config.site}/post/${item.ca_data_id}`">
+                    {{ item.data.title }}
+                  </a>
+                </template>
+                <template v-if="['followActivity'].includes(item.ca_module_type)">
+                  <template v-if="['like_article', 'release_article'].includes(item.data.key)">
+                    <a :href="`${config.site}/post/${item.ca_data_id}`">
+                      {{ item.data.data.title }}
+                    </a>
+                  </template>
+                </template>
+              </div>
 
-          <div class="text-[13px] flex gap-2">
-            <template v-if="['index', 'matrix'].includes(item.ca_module_type)">
-              <a :href="`${config.site}/u/${item.data.author.slug}/updates`">{{ item.data.author.nickname }}</a>
-              <template v-if="item.data.is_matrix">
+              <div class="text-[13px] flex gap-2">
+                <template v-if="['index', 'matrix'].includes(item.ca_module_type)">
+                  <a :href="`${config.site}/u/${item.data.author.slug}/updates`">{{ item.data.author.nickname }}</a>
+                  <template v-if="item.data.is_matrix">
+                    <span>/</span>
+                    <span>
+                      MATRIX
+                    </span>
+                  </template>
+                </template>
+                <template v-if="['followActivity'].includes(item.ca_module_type)">
+                  <a :href="`${config.site}/u/${item.data.author.slug}/updates`">{{ item.data.author.nickname }}</a> {{ transformAction(item.data.key) }}
+                  <span>/</span>
+                  <a :href="`${config.site}/u/${item.data.author.slug}/updates`">{{ item.data.author.nickname }}</a>
+                </template>
                 <span>/</span>
                 <span>
-                  MATRIX
+                  {{ dayjs(item.ca_sort_at).fromNow() }}
                 </span>
-              </template>
-            </template>
-            <template v-if="['followActivity'].includes(item.ca_module_type)">
-              <a :href="`${config.site}/u/${item.data.author.slug}/updates`">{{ item.data.author.nickname }}</a> {{ transformAction(item.data.key) }}
-              <span>/</span>
-              <a :href="`${config.site}/u/${item.data.author.slug}/updates`">{{ item.data.author.nickname }}</a>
-            </template>
-            <span>/</span>
-            <span>
-              {{ dayjs(item.ca_sort_at).fromNow() }}
-            </span>
+              </div>
+            </div>
+
+            <div class="flex-grow-0 flex items-center">
+              <img v-if="banner(item.ca_module_type, item)" class=" w-[160px] h-[100px] rounded-md" :src="banner(item.ca_module_type, item)">
+            </div>
           </div>
         </div>
-
-        <div class="flex-grow-0 flex items-center">
-          <img v-if="banner(item.ca_module_type, item)" class=" w-[160px] h-[100px] rounded-md" :src="banner(item.ca_module_type, item)">
-        </div>
       </div>
-    </div>
-  </div>
+    </template>
+  </WorldlineContent>
 </template>
